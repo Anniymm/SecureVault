6907a330d02275b76a37fb0aa4f003646101113e
Anniymm <animaisuradze1999@gmail.com>
2025-05-24 13:51:23 +0400

    Add user initial model and update settings

--- project/settings.py
+++ project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'userSystem',
-    
+
 ]
 
 MIDDLEWARE = [
@@ -123,3 +123,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+
+AUTH_USER_MODEL = 'userSystem.CustomUser'
+
--- userSystem/models.py
+++ userSystem/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    display_name = models.CharField(max_length=100, blank=True, null=True)
+    two_factor_secret = models.CharField(max_length=64, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
