c76173ec4ed99f6051fe2c6538ea9796a016c005
Anniymm <animaisuradze1999@gmail.com>
2025-05-28 21:10:29 +0400

    remove field in usersystem customuser

--- userSystem/models.py
+++ userSystem/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     display_name = models.CharField(max_length=100, blank=True, null=True)
-    two_factor_secret = models.CharField(max_length=64, blank=True, null=True)
+
 
     def __str__(self):
         return self.username
